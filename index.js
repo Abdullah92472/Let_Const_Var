2b0341520eb64608c705015e29d85c292a2fdae9
Abdullah Al Imran <abdullah.imran.ete@gmail.com>
2022-02-21 20:28:18 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+//let vs const vs var
+
+// Here I'm using variable let
+
+let a,b,c,d;
+a = 5;
+b = 6;
+c = 7;
+d = a+b+c;
+console.log(d);
+
+// Here I'm using varibale const
+
+const x = 5;
+const y = 6;
+const z = 7;
+const result = x+y+z;
+console.log(result);
+
+// Here I'm using variable var
+
+var e = 5;
+var f = 6;
+var g = 7;
+var h = e+f+g;
+console.log(h);
